3075a8a48c8fa7ead6c0bbea1385ef5fd3dc13ff
singh-harsh-03 <harshsingh14012003@gmail.com>
2024-08-21 18:42:10 +0530

    utils created

--- src/index.js
+++ src/index.js
@@ -7,7 +7,16 @@ dotenv.config({
     path: '../env'
 })
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running on port ${process.env.PORT || 8000}`)
+    })
+})
+.catch((err) => {
+    console.log("MONGODB connection failed !!!", err);
+    
+})
 /*
 import express from "express";
 const app = express()
